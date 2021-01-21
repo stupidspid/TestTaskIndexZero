068dece79c311fbfac5863699403ff97e5bdcd81
stupidspid <52347018+stupidspid@users.noreply.github.com>
2021-01-21 13:27:33 +0300

    created restriction for spawning objects

--- Index Zero/Assets/Scripts/Main/GameController.cs
+++ Index Zero/Assets/Scripts/Main/GameController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
 public class GameController : MonoBehaviour
@@ -16,11 +17,22 @@ public class GameController : MonoBehaviour
     }
     public void onClick()
     {
+        if (simplePool.isActive)
+        { 
+            StartCoroutine(SpawnMovingObject());
+            simplePool.isActive = false;
+        }
+    }
+
+    private IEnumerator SpawnMovingObject()
+    {     
         score++;
         simplePool.GetPoolObject().transform.position = Vector3.zero;
         simplePool.GetPoolObject().GetComponent<MeshFilter>().mesh = meshData._Mesh;
         simplePool.GetPoolObject().GetComponent<MeshCollider>().sharedMesh = meshData._Mesh;
         simplePool.GetPoolObject().SetActive(true);
         scoreText.text = score.ToString();
+        yield return new WaitForSeconds(5f);
+        simplePool.isActive = true;
     }
 }
--- Index Zero/Assets/Scripts/Main/SimplePool.cs
+++ Index Zero/Assets/Scripts/Main/SimplePool.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
@@ -8,6 +9,7 @@ public class SimplePool
     [Inject]
     private MovingObjectFactory movingObject;
     public List<GameObject> movingObjects = new List<GameObject>();
+    public bool isActive = true;
 
     public void CreateMovingObject()
     {
